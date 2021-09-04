3fd55bfdbfbc449be8839b5727f537eae12d013d
Syed Sibtain <85342058+SyedSibtainRazvi@users.noreply.github.com>
2021-09-05 00:45:33 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,60 @@
+import React, { useState } from "react";
+import "./styles.css";
+
+const emojiDictionary = {
+  "😀": "Grinning",
+  "😉": "Winking",
+  "😂": "Laughing",
+  "😊": "Smiling",
+  "😴": "Sleeping",
+  "😭": "Crying",
+  "😑": "Expressionless",
+  "😏": "Smirking",
+  "😡": "Angry",
+  "🤮": "Sick",
+  "🤔": "Thinking",
+  "🤥": "Lying"
+};
+
+const emojiArray = Object.keys(emojiDictionary);
+
+export default function App() {
+  const [meaning, setMeaning] = useState("");
+  function emojiInputHandler(event) {
+    var userInput = event.target.value;
+
+    var meaning = emojiDictionary[userInput];
+
+    if (meaning === undefined) {
+      meaning = "We do not have this in databade";
+    }
+
+    setMeaning(meaning);
+  }
+  function emojiClickHandler(emoji) {
+    var meaning = emojiDictionary[emoji];
+    setMeaning(meaning);
+  }
+  return (
+    <div className="App">
+      <h1 style={{ color: "darkblue" }}>Emojeeeeez</h1>
+
+      <input onChange={emojiInputHandler} />
+
+      <h2>{meaning}</h2>
+
+      <h3> Emoji we know </h3>
+      {emojiArray.map(function (emoji) {
+        return (
+          <span
+            onClick={() => emojiClickHandler(emoji)}
+            style={{ fontSize: "2rem", padding: "0.5rem", cursor: "pointer" }}
+          >
+            {" "}
+            {emoji}{" "}
+          </span>
+        );
+      })}
+    </div>
+  );
+}
